9f2de98ef4c3911a2bcf400a516074e56e0fed26
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-08-21 14:03:55 +0900

    pingService getDetail 오류 수정

--- src/main/java/com/picmap/app/ping/PingService.java
+++ src/main/java/com/picmap/app/ping/PingService.java
@@ -26,6 +26,10 @@ public class PingService {
 	public Long savePingNum() throws Exception {
 		return pingDAO.savePingNum();
 	}
+	
+	public PingDTO getDetail(PingDTO pingDTO) throws Exception {
+		return pingDAO.getDetail(pingDTO);
+	}
 
 	// 지번으로 핑 검색
 	public Map<String, Object> getPingList(PingDTO pingDTO) throws Exception {
